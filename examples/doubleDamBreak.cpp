7d559fce6b70415f182f5da27fae324983552cb6
pierre-luc manteaux <pierreluc.manteaux@gmail.com>
2015-04-27 19:59:52 +0200

    Update : high resolution double dam break example

--- examples/doubleDamBreak.cpp
+++ examples/doubleDamBreak.cpp
@@ -14,19 +14,19 @@ using namespace hokusai;
 
 int main()
 {
-    int resolution = 40000; ///particle number per m3
+    int resolution = 290000; ///particle number per m3
     System sph(resolution);
 
-    Vec fluidBox1(0.5,0.5,0.5);
+    Vec fluidBox1(0.5,1.0,0.5);
     Vec fluidOffset1(0,0,0);
     sph.addParticleBox(fluidOffset1, fluidBox1);
 
-    Vec fluidBox2(0.5,0.5,0.5);
+    Vec fluidBox2(0.5,1.0,0.5);
     Vec fluidOffset2(1.0,0.0,0.5);
     sph.addParticleBox(fluidOffset2, fluidBox2);
 
     Vec securityOffset(1.05*sph.getSmoothingRadius());
-    Vec boundBox(1.5,2.5,1.0);
+    Vec boundBox(1.5,1.0,1.0);
     boundBox += securityOffset;
     Vec boundOffset = fluidOffset1;
     boundOffset -= securityOffset;
@@ -35,16 +35,16 @@ int main()
     sph.init();
 
     sph.getViscosity() = 0.01;
-    sph.getFluidCohesion() = 0.5;
-    sph.getBoundaryAdhesion() = 0.001;
+    sph.getFluidCohesion() = 1.0;
+    sph.getBoundaryAdhesion() = 0.01;
     sph.getBoundaryFriction() = 0.01;
-    sph.getTimeStep() = 2e-3;
+    sph.getTimeStep() = 1e-3;
 
     std::cout << "Viscosity : " << sph.getViscosity() << std::endl;
     std::cout << "Cohesion : " << sph.getFluidCohesion() << std::endl;
     std::cout << "Adhesion : " << sph.getBoundaryAdhesion() << std::endl;
 
-    double time = 2.0;
+    double time = 10.0;
     int count=0;
     boost::timer::auto_cpu_timer t;
     boost::progress_display show_progress( std::floor(time/sph.getTimeStep()) );
