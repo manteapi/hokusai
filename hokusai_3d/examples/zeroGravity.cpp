6d5acfc4a6b21a50a784d129727bbee78f4f3a71
pierre-luc manteaux <pierreluc.manteaux@gmail.com>
2016-07-18 21:07:14 +0200

    Fixed compilation

--- hokusai_3d/examples/zeroGravity.cpp
+++ hokusai_3d/examples/zeroGravity.cpp
@@ -47,59 +47,12 @@ int main()
 
     sph.init();
 
-    double time = 240.0;
-    int count=0;
-    int frameNumber = std::floor(time/0.016)-1;
-
-    std::string prefix="test";
-    std::string path="/media/manteapi/DATA/FluidSimulation/";
-    BlenderExporter blenderExporter(prefix, path, sph.particleNumber(), frameNumber);
-
-    boost::timer::auto_cpu_timer t;
-    boost::progress_display show_progress( std::floor(time/solverParams.timeStep()) );
-    while(sph.getTime()<=time)
-    {
-        //Simulate
-        sph.computeSimulationStep();
-
-        //Output
-        if( std::floor((sph.getTime()-solverParams.timeStep())/0.016) != std::floor(sph.getTime()/0.016) )
-        {
-            blenderExporter.apply(sph);
-            write_frame(sph.m_particles, count);
-            ++count;
-        }
-
-        //Update progress bar
-        ++show_progress;
-    }
-
-    return 0;
-}
-
-    HReal timeStep = 1e-2;
-    int maxPressureSolveIterationNb = 2;
-    HReal maxDensityError = 1.0;
-    SolverParams solverParams(timeStep, maxPressureSolveIterationNb, maxDensityError);
-
-    System sph(fluidParams, boundaryParams, solverParams);
-
-    Vec3r  fluidBox(1.5,1.5,1.5);
-    Vec3r  fluidOffset(0,0,0);
-    Vec3r velocity(0,0,0);
-    sph.addParticleBox(fluidOffset, fluidBox, velocity);
-
-    Vec3r  gravity(0,0,0);
-    sph.setGravity(gravity);
-
-    sph.init();
-
     double time = 60.0;
     int count=0;
     int frameNumber = std::floor(time/0.016)-1;
 
     std::string prefix="test";
-    std::string path="/media/manteapi/49534769-eb68-4ca6-a09a-7e173f850b03/Hokusai/ZeroGravity/"
+    std::string path="/media/manteapi/49534769-eb68-4ca6-a09a-7e173f850b03/Hokusai/ZeroGravity/";
     BlenderExporter blenderExporter(prefix, path, sph.particleNumber(), frameNumber);
 
     boost::timer::auto_cpu_timer t;
